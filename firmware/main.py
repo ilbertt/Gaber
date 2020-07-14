b8b2d6cd43a8e5abeb9440f1dc44bdb13b70d46b
Luca8991 <luca.berte98@hotmail.it>
2020-07-14 14:21:46 +0200

    wait until wifi is connected

--- firmware/main.py
+++ firmware/main.py
@@ -6,6 +6,10 @@ adress="192.168.0.8"
 connport=1234
 wlan=network.WLAN()
 mac=wlan.config('mac')
+
+while not wlan.isconnected():
+    pass
+
 so=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 so.connect((adress, connport))
 so.send(mac)
