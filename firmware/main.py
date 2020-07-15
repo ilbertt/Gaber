adb0f591fcc5059f83e1eff6dde477a73cac18cf
massimoalbarello <albo.max@hotmail.it>
2020-07-15 17:56:23 +0200

    comments and user update

--- firmware/main.py
+++ firmware/main.py
@@ -3,18 +3,29 @@ import socket
 import gbros_fw
 
 wlan=network.WLAN()
+
+# get esp mac address
 mac=wlan.config('mac')
 
 while not wlan.isconnected():
     pass
 
+# configuration port for new client
 connport=50500
+
+# server address
 adress="192.168.0.8"
 #adress = socket.getaddrinfo("server.domain.com",connport)[0][-1][0]
 
 so=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 so.connect((adress, connport))
 so.send(mac)
+
+# get communication port from server
 port=int(so.read())
+
+# close configuration socket
 so.close()
+
+# run client firmware with communication port
 gbros_fw.run((adress, port))
