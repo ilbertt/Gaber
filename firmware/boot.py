476c27c0e8f14b9dc5a543b39c8a7e72ebc2ef2f
massimoalbarello <albo.max@hotmail.it>
2020-07-17 17:05:36 +0200

    connect on boot

--- firmware/boot.py
+++ firmware/boot.py
@@ -1,9 +1,12 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 #import esp
 #esp.osdebug(None)
-import uos, machine, sh1106
+#import uos
 #uos.dupterm(None, 1) # disable REPL on UART(0)
 import gc
 #import webrepl
 #webrepl.start()
 gc.collect()
+
+import wificonnect
+do_connect()
