41670cd80e1bbc86972cc8f4bff3d3b479ae7210
gabriele bonanno <gigo.bon@mail.com>
2020-07-22 11:30:39 +0200

    commit

--- src/app.py
+++ src/app.py
@@ -36,6 +36,7 @@ class Application:
 		self.outPins = {}
 		self.pwmPins= {}
 		self.img_new=False
+		self.notifyStarted=False
 		
 		self.dispList= {"sh1106":0, "ssd1306":1}	
 
@@ -93,53 +94,68 @@ class Application:
 	def setNeoPin(self, pin):
 		self.neoPins.append(pin)
 
-	def setOutPin(self, pin, value):
-		self.__recv()
-		self.__send(str(pin*10+int(not value)).zfill(3).encode())
+	def setOutPin(self, pin, value, notify=False):
+		if((self.notifyStarted and notify) or (not self.notifyStarted)):
+			self.__recv()
+			self.__send(str(pin*10+int(not value)).zfill(3).encode())
+
 		time.sleep(0.01)
 
-	def setInPin(self, pin):
-		self.__recv()
-		self.__send(str(pin).zfill(2).encode())
+	def setInPin(self, pin, notify=False):
+		if((self.notifyStarted and notify) or (not self.notifyStarted)):
+			self.__recv()
+			self.__send(str(pin).zfill(2).encode())
+
 		time.sleep(0.01)
 
-	def setDisplay(self, sda, scl, heigth, width, dispType):
-		self.heigth = heigth
-		self.width = width
-		disp=self.dispList[dispType]
-		self.__recv()
-		self.__send((str(sda).zfill(2)+str(scl).zfill(2)+str(heigth).zfill(3)+str(width).zfill(3)+str(disp).zfill(2)).encode())
+	def setDisplay(self, sda, scl, heigth, width, dispType, notify=False):
+		if((self.notifyStarted and notify) or (not self.notifyStarted)):
+			self.heigth = heigth
+			self.width = width
+			disp=self.dispList[dispType]
+			self.__recv()
+			self.__send((str(sda).zfill(2)+str(scl).zfill(2)+str(heigth).zfill(3)+str(width).zfill(3)+str(disp).zfill(2)).encode())
+		
 		time.sleep(0.01)
 
-	def setPwm(self, pin, freq, duty):
-		self.__recv()
-		self.__send((str(pin).zfill(2)+str(freq).zfill(3)+str(duty).zfill(4)).encode())
+	def setPwm(self, pin, freq, duty, notify=False):
+		if((self.notifyStarted and notify) or (not self.notifyStarted)):
+			self.__recv()
+			self.__send((str(pin).zfill(2)+str(freq).zfill(3)+str(duty).zfill(4)).encode())
+		
 		time.sleep(0.01)
 
-	def readAdc(self, pin, resolution=1024):
-		self.__recv()
-		self.__send(str(pin).zfill(4))
-		time.sleep(0.01)
-		data=int(self.__recv())%resolution
-		self.__send(b'')
+	def readAdc(self, pin, resolution=1024, notify=False):
+		data=0
+		if((self.notifyStarted and notify) or (not self.notifyStarted)):
+			self.__recv()
+			self.__send(str(pin).zfill(4))
+			time.sleep(0.01)
+			data=int(self.__recv())%resolution
+			self.__send(b'')
+
 		time.sleep(0.01)
 		return data
 		
-	def setNeopixel(self, status, pin=-1):
-		if(len(self.neoPins)):
-			self.__recv()
-			if(pin==-1):
-				self.__send((str(self.neoPins[0]).zfill(2)+str(status[0]).zfill(3)+str(status[1]).zfill(3)+str(status[2]).zfill(3)).encode())
-			elif(pin in self.neoPins):
-				self.__send((str(pin).zfill(2)+str(status[0]).zfill(3)+str(status[1]).zfill(3)+str(status[2]).zfill(3)).encode())
-			else:
-				self.__send(b'')
+	def setNeopixel(self, status, pin=-1, notify=False):
+		if((self.notifyStarted and notify) or (not self.notifyStarted)):
+			if(len(self.neoPins)):
+				self.__recv()
+				if(pin==-1):
+					self.__send((str(self.neoPins[0]).zfill(2)+str(status[0]).zfill(3)+str(status[1]).zfill(3)+str(status[2]).zfill(3)).encode())
+				elif(pin in self.neoPins):
+					self.__send((str(pin).zfill(2)+str(status[0]).zfill(3)+str(status[1]).zfill(3)+str(status[2]).zfill(3)).encode())
+				else:
+					self.__send(b'')
 
 			time.sleep(0.01)
 
-	def recvData(self):
-		data=self.__recv()
-		self.__send(b'')
+	def recvData(self, notify=False):
+		data=0
+		if((self.notifyStarted and notify) or (not self.notifyStarted)):
+			data=self.__recv()
+			self.__send(b'')
+
 		for pin in self.inPins:
 			if(data & 1<<self.inPins[pin]["number"]):
 				self.buttons[pin]=1
@@ -150,37 +166,41 @@ class Application:
 		#print(self.buttons)
 		return self.buttons
 
-	def sendImg_and_recvData(self):
-		if (self.config["rotation"]):
-			pic=self.img
-		else:
-			pic=self.img.rotate(180)
+	def sendImg_and_recvData(self, notify=False):
+		data=0
+		if((self.notifyStarted and notify) or (not self.notifyStarted)):
+			if (self.config["rotation"]):
+				pic=self.img
+			else:
+				pic=self.img.rotate(180)
 
-		if(self.config["contrast"] and not(self.ispic)):
-			pic=ImageOps.colorize(pic, (255,255,255), (0,0,0))
-		
-		if(self.ispic):
-			self.ispic=False
+			if(self.config["contrast"] and not(self.ispic)):
+				pic=ImageOps.colorize(pic, (255,255,255), (0,0,0))
+			
+			if(self.ispic):
+				self.ispic=False
 
-		
-		pic=pic.convert('1')
-		pic=pic.tobytes()
-		self.data=self.__recv()
-		#time.sleep(0.05)
-		if(self.heigth and self.width and self.img_new):
-			self.img_new=False
-			self.__send(pic[:512])
-			time.sleep(0.01)
-			self.__recv()
-			self.__send(pic[512:])
-			time.sleep(0.01)
-			self.__send(b'')
-			self.__recv()
-		else:
-			self.__send(b'')
+			
+			pic=pic.convert('1')
+			pic=pic.tobytes()
+			data=self.__recv()
+			#time.sleep(0.05)
+			if(self.heigth and self.width and self.img_new):
+				self.img_new=False
+				l=int(len(pic)/2)
+				self.__send(pic[:l])
+				time.sleep(0.01)
+				self.__recv()
+				self.__send(pic[l:])
+				time.sleep(0.01)
+				self.__send(b'')
+				self.__recv()
+			else:
+				self.__send(b'')
+
 
 		for pin in self.inPins:
-			if(self.data & 1<<self.inPins[pin]["number"]):
+			if(data & 1<<self.inPins[pin]["number"]):
 				self.buttons[pin]=1
 			else:
 				self.buttons[pin]=0
@@ -189,72 +209,89 @@ class Application:
 		#print(self.buttons)
 		return self.buttons
 
-	def sendImg(self):
-		if(self.heigth and self.width):
-			if (self.config["rotation"]):
-				pic=self.img
-			else:
-				pic=self.img.rotate(180)
+	def sendImg(self, notify=False):
+		if((self.notifyStarted and notify) or (not self.notifyStarted)):
+			if(self.heigth and self.width):
+				if (self.config["rotation"]):
+					pic=self.img
+				else:
+					pic=self.img.rotate(180)
 
-			if(self.config["contrast"] and not(self.ispic) ):
-				pic=ImageOps.colorize(pic, (255,255,255), (0,0,0))
-			
-			if(self.ispic):
-				self.ispic=False
-
-			if(self.img_new):
-				self.img_new=False
-				pic=pic.convert('1')
-				pic=pic.tobytes()
-				self.__recv()
-				self.__send(pic[:512])
-				time.sleep(0.01)
-				self.__recv()
-				self.__send(pic[512:])
-				time.sleep(0.01)
-				self.__send(b'')
-				self.__recv()
+				if(self.config["contrast"] and not(self.ispic) ):
+					pic=ImageOps.colorize(pic, (255,255,255), (0,0,0))
 				
-			time.sleep(0.01)
+				if(self.ispic):
+					self.ispic=False
 
-	def setText(self,pos,txt, txt_color, txt_font):
-		self.img_new=True
-		self.d.text(pos, txt, txt_color,font=txt_font)
+				if(self.img_new):
+					self.img_new=False
+					pic=pic.convert('1')
+					pic=pic.tobytes()
+					l=int(len(pic)/2)
+					self.__recv()
+					self.__send(pic[:l])
+					time.sleep(0.01)
+					self.__recv()
+					self.__send(pic[l:])
+					time.sleep(0.01)
+					self.__send(b'')
+					self.__recv()
+				
+		
+		time.sleep(0.01)
 
-	def setContrast(self, contrast):
-		self.img_new=True
-		self.config["contrast"]=contrast
-		if(self.confpath):
-			with open(self.confpath, "w") as rf:
-				json.dump(self.config, rf)
+	def setText(self,pos,txt, txt_color, txt_font, notify=False):
+		if((self.notifyStarted and notify) or (not self.notifyStarted)):
+			self.img_new=True
+			self.d.text(pos, txt, txt_color,font=txt_font)
 
-	def setRotation(self, rotation):
-		self.img_new=True
-		self.config["rotation"]=rotation
-		if(self.confpath):
-			with open(self.confpath, "w") as rf:
-				json.dump(self.config, rf)
+	def setContrast(self, contrast, notify=False):
+		if((self.notifyStarted and notify) or (not self.notifyStarted)):
+			self.img_new=True
+			self.config["contrast"]=contrast
+			if(self.confpath):
+				with open(self.confpath, "w") as rf:
+					json.dump(self.config, rf)
+
+	def setRotation(self, rotation, notify=False):
+		if((self.notifyStarted and notify) or (not self.notifyStarted)):
+			self.img_new=True
+			self.config["rotation"]=rotation
+			if(self.confpath):
+				with open(self.confpath, "w") as rf:
+					json.dump(self.config, rf)
 
 	def getFonts(self):
 		return self.fonts
 
-	def setImg(self, img):
-		self.img_new=True
-		self.img=img.convert('L')
-		self.ispic = True
+	def setImg(self, img, notify=False):
+		if((self.notifyStarted and notify) or (not self.notifyStarted)):
+			self.img_new=True
+			self.img=img.convert('L')
+			self.ispic = True
 
 	def addFont(self, font, font_size):
 		self.fonts.append(ImageFont.truetype(font, font_size))
 
-	def newImg(self):
-		self.img_new=True
-		self.img=Image.new("L",(self.heigth,self.width))
-		self.d=ImageDraw.Draw(self.img)
-		self.d.rectangle((0,0,self.heigth,self.width),fill=0)
+	def newImg(self, notify=False):
+		if((self.notifyStarted and notify) or (not self.notifyStarted)):
+			self.img_new=True
+			self.img=Image.new("L",(self.heigth,self.width))
+			self.d=ImageDraw.Draw(self.img)
+			self.d.rectangle((0,0,self.heigth,self.width),fill=0)
 	
-	def fillImg(self, img_color):
-		self.img_new=True
-		self.d.rectangle((0,0,self.heigth,self.width),fill=img_color)
+	def fillImg(self, img_color, notify=False):
+		if((self.notifyStarted and notify) or (not self.notifyStarted)):
+			self.img_new=True
+			self.d.rectangle((0,0,self.heigth,self.width),fill=img_color)
 	
 	def getIpAddress(self):
 		return self.address[0]
+
+	def startNotify(self):
+		self.notifyStarted=True
+		self.newImg()
+
+	def stopNotify(self):
+		self.notifyStarted=False
+		self.newImg()
