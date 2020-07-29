d990d21f16465b78fcefa3b8620d540a4c465f5d
Luca8991 <luca.berte98@hotmail.it>
2020-07-29 23:13:21 +0200

    console streaming and tv_livingroom

--- src/iot_funct/screen.py
+++ src/iot_funct/screen.py
@@ -1,17 +1,20 @@
 import time
 import threading
-import sys
+import os
 
 class Screen(threading.Thread):
     def __init__(self, username, device):
-        self.state = False
         self.device = device
         self.username = username
+        self.counter = 0
+        self.started = False
+        self.deviceChanged = False
 
     def __startThread(self):
         threading.Thread.__init__(self)
         self.name = self.username+"_screen"
         self.start()
+        self.started=True
 
     def getNotificationMessage(self, device, username=None):
 
@@ -19,8 +22,6 @@ class Screen(threading.Thread):
 
         msg.append([(10, 35), device.getName().replace("_", " ").upper()])
         msg_status = "START STREAM?"
-        if self.state:
-            msg_status = "STOP STREAM?"
         
         msg.append([(10,50), msg_status])
 
@@ -28,25 +29,40 @@ class Screen(threading.Thread):
     
     def run(self):
         startTime = time.time()
-        counter = 0
 
-        while self.state:
+        commands = ['ls', 'vcgencmd measure_temp', 'ifconfig']
+        i=0
+
+        while True:
             if self.device:
                 self.device.stream = True
-                if time.time() - startTime > 1:
+                self.device.isNear = True
+
+                if time.time() - startTime > 3 or self.deviceChanged:
+                    self.deviceChanged = False
                     startTime = time.time()
-                    counter += 1
                     self.device.newImg()
-                    self.device.setText((45,0),str(counter), 255, self.device.getFonts()[1])
+                    self.device.setText((35,0),"CONSOLE ", 255,self.device.getFonts()[0])
+                    self.device.setText((1,10),">", 255,self.device.getFonts()[0])
+                    self.device.setText((10,10), commands[i], 255,self.device.getFonts()[0])
+                    stream = os.popen(commands[i])
+                    output = stream.read()
+                    self.device.setText((10,20), output, 255, self.device.getFonts()[0])
                     self.device.sendImg()
-        self.device = 0
-        sys.exit(0)
+                    i+=1
+                    if i > len(commands)-1:
+                        i=0
     
     def handleStreaming(self, device):
-        self.state = not self.state
-        if self.state:
-            self.__startThread()
+        if self.device!=device:
+            old_device = self.device
             self.__setDevice(device)
+            self.deviceChanged = True
+            if old_device != 0:
+                old_device.stream = False
+                old_device.isNear = False
+            if not self.started:
+                self.__startThread()
     
     def __setDevice(self, device):
         self.device = device
